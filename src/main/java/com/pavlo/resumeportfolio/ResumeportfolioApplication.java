5dfb0ef0fe2501870484ed9f2ea91e9da5773c75
Pavlo <pavlo.orsahosh05@gmail.com>
2025-10-22 10:05:09 +0200

    Initial project setup

---
+++ src/main/java/com/pavlo/resumeportfolio/ResumeportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.pavlo.resumeportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeportfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavlo/resumeportfolio/ResumeportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavlo.resumeportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
